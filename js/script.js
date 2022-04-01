fc9fd20df9b80c3b9aa4dc0c8bb3dbca6e3d68e4
Grace Silvaroli <99274228+GraceSilvaroli@users.noreply.github.com>
2022-04-01 15:05:14 +0000

    edit

--- js/script.js
+++ js/script.js
@@ -2,18 +2,20 @@
 // Created on: March 2022
 // This file contains the JS functions for index.html
 
+
 'use strict'
+
 /**
  * This function calculates the area of the triangle.
  */
 function calculate () {
   // input
-  const base = parseInt(document.getElementById('base-of-triangle').value)
-  const height = parseInt(document.getElementById('height-of-triangle').value)
+  let base = parseInt(document.getElementById('base-of-triangle').value)
+  let height = parseInt(document.getElementById('height-of-triangle').value)
   
   // process
-  const area = base * height/2
-  const perimeter = (base * height)/2
+  let area = base * height/2
+  let perimeter = (base * height)/2
   
   // output
   document.getElementById('area').innerHTML = 'The area of the triangle is: ' + area + ' cm²'
